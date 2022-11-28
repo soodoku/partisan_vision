b6b4f5984e65c1fc78c5e2c140b9a97c896b7f09
***** <gsood07@gmail.com>
2022-11-27 17:53:37 -0800

    turk

--- scripts/turk.R
+++ scripts/turk.R
@@ -5,16 +5,38 @@ setwd(githubdir)
 setwd("partisan_vision/")
 # Load libs
 library(tidyverse)
-library(car)
-library(dplyr)
-library(xtable)
+library(knitr)
 library(readr)
+library(magrittr)
 
 ## Load data
 turk <- read_csv("data/turk/merged_survey_ip_06_29_2020_final.csv")
 
-# Drop people who didn't finish
-# turk <- turk %>% filter(!is.na(sincerity))
+# Analyze PID --- PID 7 is missing
+# table(turk$pid_3...Selected.Choice)
+# table(turk$pid_3...Other..please.specify....Text)
 
-turk$pid_dem
-turk$trump_masks
+turk %<>% mutate(pid_dem_l = case_when(pid_dem == "1" ~ "democrat",
+                                       pid_dem == "2" ~ "republican")) 
+
+turk %>% 
+  group_by(pid_dem_l) %>% 
+  summarize(p_25 = quantile(trump_masks, probs = .25, na.rm = T), 
+            p_50 = quantile(trump_masks, probs = .25, na.rm = T), 
+            p_75 = quantile(trump_masks, probs = .75, na.rm = T), 
+            n = n())
+
+
+# See if filtering on sincere respondents changes anything --- NO
+
+# Drop people who didn't finish
+turk <- turk %>% filter(!is.na(sincerity))
+
+turk %>% 
+  group_by(pid_dem_l) %>% 
+  summarize(p_25 = quantile(trump_masks, probs = .25, na.rm = T), 
+            p_50 = quantile(trump_masks, probs = .25, na.rm = T), 
+            p_75 = quantile(trump_masks, probs = .75, na.rm = T), 
+            n = n())
+
+
